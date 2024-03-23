e7e39ee57263a2b2ba3d02c209753d7d69090030
thanhhh2512 <tatrongthanh2002@gmail.com>
2024-03-23 14:53:29 +0700

    thêm trang add product

---
+++ web-nln/src/pages/Admin/product/AddProductPage.js
@@ -0,0 +1,11 @@
+import Header from '../../../component/fix/Header'
+import Footer from '../../../component/fix/Footer'
+export default function AddProductPage(){
+        return (
+            <div className = "AddProductPage">
+                <Header/>
+                <Footer/>
+
+            </div>
+        )
+}
